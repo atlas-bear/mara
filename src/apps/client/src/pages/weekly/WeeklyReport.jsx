b9ffa5be3c3d3a641f11d02df0f4589d75d5e636
rhinonix <1107968+rhinonix@users.noreply.github.com>
2025-02-23 13:49:56 +0400

    feat: Update import statements in WeeklyReport to use default imports

--- src/apps/client/src/pages/weekly/WeeklyReport.jsx
+++ src/apps/client/src/pages/weekly/WeeklyReport.jsx
@@ -1,7 +1,7 @@
-import { ExecutiveBrief } from '@shared/components/ExecutiveBrief'
-import { RegionalBrief } from '@shared/components/RegionalBrief'
-import { IncidentDetails } from '@shared/components/IncidentDetails'
-import { MaritimeMap } from '@shared/components/MaritimeMap'
+import ExecutiveBrief from '@shared/components/ExecutiveBrief'
+import RegionalBrief from '@shared/components/RegionalBrief'
+import IncidentDetails from '@shared/components/IncidentDetails'
+import MaritimeMap from '@shared/components/MaritimeMap'
 
 export default function WeeklyReport() {
   return (
