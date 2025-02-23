61f118fe01ea6f06b4fbd772de70731ef895dd20
rhinonix <1107968+rhinonix@users.noreply.github.com>
2025-02-23 14:24:47 +0400

    feat: Wrap Routes in Router component for improved routing structure

--- src/apps/client/src/App.jsx
+++ src/apps/client/src/App.jsx
@@ -1,16 +1,20 @@
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout'
 import WeeklyReport from './pages/weekly/WeeklyReport'
 
-export default function App() {
+function App() {
   return (
-    <Routes>
-      <Route path="/:yearWeek" element={<MainLayout />}>
-        <Route index element={<WeeklyReport />} />
-        {/* Future routes can be added here */}
-        {/* <Route path="/analysis" element={<Analysis />} /> */}
-        {/* <Route path="/custom-reports" element={<CustomReports />} /> */}
-      </Route>
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/:yearWeek" element={<MainLayout />}>
+          <Route index element={<WeeklyReport />} />
+          {/* Future routes can be added here */}
+          {/* <Route path="/analysis" element={<Analysis />} /> */}
+          {/* <Route path="/custom-reports" element={<CustomReports />} /> */}
+        </Route>
+      </Routes>
+    </Router>
   )
 }
+
+export default App
