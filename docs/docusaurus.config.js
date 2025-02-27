d6d68f66bae2bf3d10c8cef2cf1f8e5a838a6b9a
rhinonix <1107968+rhinonix@users.noreply.github.com>
2025-02-27 13:23:46 +0500

    feat: Update site title and logo in Docusaurus configuration

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -78,10 +78,10 @@ const config = {
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: "My Site",
+        title: "MARA Docs",
         logo: {
-          alt: "My Site Logo",
-          src: "img/logo.svg",
+          alt: "MARA Logo",
+          src: "img/mara_logo.svg",
         },
         items: [
           {
@@ -92,7 +92,7 @@ const config = {
           },
           { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/atlas-bear/mara",
             label: "GitHub",
             position: "right",
           },
