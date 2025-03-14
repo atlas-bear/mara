a6fbe7430162cae305ddb98cd8b70dfab89120cb
rhinonix <1107968+rhinonix@users.noreply.github.com>
2025-03-14 15:50:21 +0100

    Fix email subject location name retrieval to use cached data

--- functions/send-flash-report.js
+++ functions/send-flash-report.js
@@ -1007,6 +1007,7 @@ export const handler = async (event, context) => {
 
           // Create email subject in the new format
           const locationName =
+            cachedData.location?.name ||
             preparedIncident.location?.name ||
             preparedIncident.region ||
             "Unknown Location";
